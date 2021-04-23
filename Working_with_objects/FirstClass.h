d400bd3af0a35673e5d1dbfd46aec71d34b0561d
Pirate1953 <piratefromnev1953@gmail.com>
2021-04-23 19:34:14 +0300

    Task 3.2

--- Working_with_objects/FirstClass.h
+++ Working_with_objects/FirstClass.h
@@ -9,10 +9,14 @@ class FirstClass : public BaseClass
 public:
 	using BaseClass::BaseClass; //Using members of base class 
 
-	/*Starts algorithm of setting data and hierarchical tree construction*/
+	/**
+	*	Starts algorithm of setting data and hierarchical tree construction
+	*/
 	void start();
 
-	/*Prints result, calls 'show' method*/
+	/**
+	*	Prints result, calls 'show' method
+	*/
 	void printResults();
 };
 
